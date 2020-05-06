f75bacb2b12d10b149da82430c9cca2e7269cc95
David Flores <dmousex@gmail.com>
2020-05-05 20:38:47 -0500

    remove deprecation RegistryInterface

--- src/Repository/SkillGroupRepository.php
+++ src/Repository/SkillGroupRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\SkillGroup;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SkillGroupRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, SkillGroup::class);
     }
