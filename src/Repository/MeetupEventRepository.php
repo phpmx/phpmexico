950b2a63c2e9d6d054c1a096d8a5ec952f0e466e
Javier Ledezma <jledezma@medtrainer.com>
2020-07-17 11:33:15 -0500

    Remove unused MeetupEvent repository methods

--- src/Repository/MeetupEventRepository.php
+++ src/Repository/MeetupEventRepository.php
@@ -18,33 +18,4 @@ class MeetupEventRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, MeetupEvent::class);
     }
-
-    // /**
-    //  * @return MeetupEvent[] Returns an array of MeetupEvent objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?MeetupEvent
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
