336eba0cd47244e00a3edb47b5f2577b83b21e87
Juan Carlos Juarez <juan.juarez@linio.com>
2020-11-09 20:45:17 -0600

    Apply style coding rules

--- src/Migrations/Version20201110021650.php
+++ src/Migrations/Version20201110021650.php
@@ -12,23 +12,23 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201110021650 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE meetup_event ADD linkedin_url VARCHAR(255) DEFAULT NULL, ADD slide_url VARCHAR(255) DEFAULT NULL, ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, ADD updated_at DATETIME DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE meetup_event DROP linkedin_url, DROP slide_url, DROP created_at, DROP updated_at');
     }
