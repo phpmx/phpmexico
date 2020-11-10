9704412914365c1e05733541f5611a691b2f6b3d
Juan Carlos Juarez <juan.juarez@linio.com>
2020-11-09 20:25:04 -0600

    Add migration for add new fields for meetup event

---
+++ src/Migrations/Version20201110021650.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201110021650 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meetup_event ADD linkedin_url VARCHAR(255) DEFAULT NULL, ADD slide_url VARCHAR(255) DEFAULT NULL, ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, ADD updated_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meetup_event DROP linkedin_url, DROP slide_url, DROP created_at, DROP updated_at');
+    }
+}
