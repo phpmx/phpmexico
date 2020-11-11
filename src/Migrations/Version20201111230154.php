df90936350e8d3d6e8f9829e88ebaf1a2cfed244
Juan Carlos Juarez <juan.juarez@linio.com>
2020-11-11 17:55:25 -0600

    Apply style coding rules

--- src/Migrations/Version20201111230154.php
+++ src/Migrations/Version20201111230154.php
@@ -12,23 +12,23 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201111230154 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE meetup_event ADD youtube_url VARCHAR(255) DEFAULT NULL, ADD twitter_url VARCHAR(255) DEFAULT NULL, ADD facebook_url VARCHAR(255) DEFAULT NULL, ADD slide_url VARCHAR(255) DEFAULT NULL, ADD speaker_linkedin_url VARCHAR(255) DEFAULT NULL, ADD speaker_git_url VARCHAR(255) DEFAULT NULL, ADD speaker_twitter_url VARCHAR(255) DEFAULT NULL, ADD speaker_facebook_url VARCHAR(255) DEFAULT NULL, ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, ADD updated_at DATETIME DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE meetup_event DROP youtube_url, DROP twitter_url, DROP facebook_url, DROP slide_url, DROP speaker_linkedin_url, DROP speaker_git_url, DROP speaker_twitter_url, DROP speaker_facebook_url, DROP created_at, DROP updated_at');
     }
